914fca08bde5d14a7f5e8c18dee941a526d8414f
Faizal Shahul Hamed <fsl@tpcdevsvr.com>
2024-10-12 14:12:38 +0530

    Updated the docker file for frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,6 @@ app.use('/api', productRoutes);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
